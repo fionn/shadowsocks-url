6c3fb1cba8b27669ddde5f0d1542a0d839c9de24
Fionn Fitzmaurice <fionn@github.com>
2020-10-09 16:50:00 +0800

    Lint

--- ss_url.py
+++ ss_url.py
@@ -1,59 +1,71 @@
 #!/usr/bin/env python3
+"""Shadowsocks URLs"""
 
+import sys
 import json
 import base64
 import argparse
-import qrcode
+from typing import Dict
+from pathlib import Path
 
-def get_vars(path):
-    configuration = open(path, "r").read()
-    return json.loads(configuration)
+import qrcode #type: ignore
+from qrcode.image.svg import SvgPathImage #type: ignore
 
-def ss_url(q, password = True):
-    url = ""
-    if not password:
-        q["password"] = ""
+def get_config(path: Path) -> Dict[str, str]:
+    """Return the config as a dict"""
+    with path.open() as config:
+        return json.loads(config.read())
 
-    url = q["method"] + ":" + q["password"] + "@" \
-          + q["server"] + ":" + str(q["server_port"])
+def ss_url(config: Dict[str, str], exclude_password: bool = True) -> str:
+    """Build the ss:// URL"""
+    if exclude_password:
+        config["password"] = ""
+
+    url = config["method"] + ":" + config["password"] + "@" \
+          + config["server"] + ":" + str(config["server_port"])
 
     return "ss://" + base64.b64encode(bytes(url, "ascii")).decode()
 
-def ss_qr(url):
+def ss_qr(url: str) -> qrcode.QRCode:
+    """Make the QRCode object"""
+    # pylint: disable=invalid-name
     qr = qrcode.QRCode()
     qr.add_data(url)
     return qr
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "Generate Shadowsocks URL from configuration")
-    parser.add_argument("config", help = "Path to configuration file")
-    parser.add_argument("-q", "--qr", help = "Print QR code",
-                        action = "store_true")
-    parser.add_argument("-n", "--nopass", help = "Exclude password field",
-                        action = "store_false")
-    parser.add_argument("-p", "--png", help = "Save QR code as PNG",
-                        action = "store_true")
-    parser.add_argument("-s", "--svg", help = "Save QR code as SVG",
-                        action = "store_true")
+def main() -> None:
+    """Entry point"""
+    parser = argparse.ArgumentParser(description="Generate Shadowsocks URL from configuration")
+    parser.add_argument("config", help="Path to configuration file", type=Path)
+    parser.add_argument("-q", "--qr", help="Print QR code",
+                        action="store_true")
+    parser.add_argument("-n", "--nopass", help="Exclude password field",
+                        action="store_true")
+    parser.add_argument("-p", "--png", help="Save QR code as PNG",
+                        action="store_true")
+    parser.add_argument("-s", "--svg", help="Save QR code as SVG",
+                        action="store_true")
 
     args = parser.parse_args()
 
-    q = get_vars(args.config)
-    url = ss_url(q, password = args.nopass)
+    config = get_config(args.config)
+    url = ss_url(config, exclude_password=args.nopass)
     print(url)
 
-    if args.qr:
-        qr = ss_qr(url)
-        qr.print_ascii(tty = True)
-    if args.png:
-        path = q["server"] + ".png"
-        qr = ss_qr(url)
-        qr.make_image().save(path)
-        print("Saved to", path)
-    if args.svg:
-        from qrcode.image.svg import SvgPathImage as svg_image
-        path = q["server"] + ".svg"
-        qr = ss_qr(url)
-        img = qr.make_image(svg_image).save(path)
-        print("Saved to", path)
+    qr = ss_qr(url) # pylint: disable=invalid-name
 
+    if args.qr:
+        qr.print_ascii(tty=True)
+
+    if args.png:
+        path = config["server"] + ".png"
+        qr.make_image().save(path)
+        print(f"Saved to {path}", file=sys.stderr)
+
+    if args.svg:
+        path = config["server"] + ".svg"
+        qr.make_image(SvgPathImage).save(path)
+        print(f"Saved to {path}", file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
